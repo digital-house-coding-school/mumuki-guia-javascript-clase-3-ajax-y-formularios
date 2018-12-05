d7495214075ec4f7c181bc949af06bac550cffe9
fedescarpa+mumuki@gmail.com <fedescarpa+mumuki@gmail.com>
2018-12-05 12:42:34 -0500

    Mumuki Export on 2018-12-05 12:42:34 -0500

--- 00003_Consumiendo APIs/test..js
+++ 00003_Consumiendo APIs/test..js
@@ -3,17 +3,24 @@
   
   _nock_.cleanAll();
   
-  const mockedGet = _nock_("https://foo.bar")
-    .get("/baz")
+  const mockedGet = _nock_("https://dev.digitalhouse.com")
+    .get("/api/getCursoss")
     .reply(200, {
-      "data": "HOLA"
+      "data": [
+        {
+          "curso":"Desarrollo Web Full Stack",
+          "id":1,
+          "descripcion":"Lorem Ipsum"
+        }
+      ]
     });
     
   _dispatch_('load', document);
   
   _wait_for_(() => mockedGet.isDone(), () => {
-    document.body.innerHTML.should.be.eql("HOLA");
+    document.body.innerHTML.should.be.eql("Lorem Ipsum");
   });
-})/*#tests>*/
+  
+});/*#tests>*/
 /*<options#*/output_ignore_scripts: true
 output_ignore_styles: true/*#options>*/
